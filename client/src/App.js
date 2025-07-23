4680e5053e8ef949c9f4295e1b251b057fa4178f
Vozhdai Oleksandr <oleksander.vozhdai@gmail.com>
2025-07-23 22:19:39 +0300

    initial app interface

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,14 @@
 import './App.css';
-
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Todo from './components/Todo';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
   return (
     <>
-
-      <nav className="navbar bg-body-tertiary">
-        <div>
-        <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Main Menu
-                </a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="/">Home</a></li>
-                  <li><a className="dropdown-item" href="/Todo">page2</a></li>
-                  <li><a className="dropdown-item" href="/">page3</a></li>
-                </ul>
-        </div>
-      </nav>
-
-      <p />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Todo" element={<Todo />}/>
-        </Routes>
-      </BrowserRouter>
+      <Todo />
     </>
   );
 }
--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,14 +1,70 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 
-axios.get('https://localhost:7052/api/ListTodo').then(res=>console.log(res.data));
-
+axios.get('https://localhost:7052/api/ListTodo').then(result=>(console.log(result)));
+    
 const Todo =() =>{
-    return(
-        <>
-        
-        </>
-    )
+
+    const [todos, setTodos] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://localhost:7052/api/ListTodo')
+            .then(response => {
+                setTodos(response.data);
+            })
+            .catch(error => {
+                console.error("Error fetching todos:", error);
+            });
+    }, []);
+    
+    
+
+    return (
+    <>
+        <div style={{width:"100%", display:"flex", flexDirection:"column"}}>
+            <div className="central-container">
+
+            <div style={{display:"flex", flexDirection:"row"}}>
+                <input placeholder="Add a new task" className="todo-input" />
+                <button className="add-task-button" > + </button>   
+            </div>
+
+            <div className="Todo-text-container">
+                <h1 className="Todo-text">Tasks to do</h1></div>
+                <div className="central-container">
+                    <ul>
+                        {todos.filter(todo=> !todo.isCompleted).map((todo)=> 
+                            <div className="Todo-container" key={todo.id}>
+                                <p>{todo.title}</p>
+                                <div style={{marginLeft:"auto", marginRight:"25px", marginTop:"auto", marginBottom:"auto"}}>
+                                    <button className="Todo-container-button" style={{marginRight:"0"}}> <i class="bi bi-check-lg"></i> </button>
+                                    <button className="Todo-container-button"> <i class="bi bi-trash3-fill"></i> </button>
+                                </div>
+                            </div>
+                        )}
+                    </ul>  
+                </div>
+                <h1 className="Todo-text">Done</h1>
+                <ul>
+                    {todos.filter(todo=> todo.isCompleted).map((todo)=>
+                        <div className="Todo-container" key={todo.id}>
+                            <p className="completed-text">
+                                <span className="no-strike">
+                                    <i className="bi bi-check-circle-fill"></i>
+                                </span>
+                                {todo.title}
+                            </p>
+                            <div style={{marginLeft:"auto", marginRight:"25px", marginTop:"auto", marginBottom:"auto"}}>
+                                <button className="Todo-container-button-rotate" style={{marginRight:"0"}}> <i class="bi bi-arrow-repeat"></i> </button>
+                                <button className="Todo-container-button"> <i class="bi bi-trash3-fill"></i> </button>
+                            </div>
+                        </div>
+                    )}
+                </ul>
+            </div>
+        </div>
+    </>
+    );
 }
 
 export default Todo;
